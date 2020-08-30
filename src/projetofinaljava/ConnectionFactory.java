09742bc2a2949a6b2c5747c3d5cec45407762e1e
Sharon Hasegawa <sharon@make.pro.br>
2020-08-30 14:28:29 -0300

    ClienteDAO e configuração git/Netbeans

--- src/projetofinaljava/ConnectionFactory.java
+++ src/projetofinaljava/ConnectionFactory.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class ConnectionFactory {
     public static Connection getConnection() throws SQLException {
-        Properties props = getProperties();
+        Properties props = getLocalProperties();
 
         String dbName = props.getProperty("database_name");
         String dbUser = props.getProperty("database_user");
@@ -22,7 +22,7 @@ public class ConnectionFactory {
         return DriverManager.getConnection(url, dbUser, dbPassword);
     }
     
-    public static Properties getProperties() {
+    public static Properties getLocalProperties() {
         Properties props = new Properties();
 	InputStream inputStream;
 
@@ -43,15 +43,19 @@ public class ConnectionFactory {
     
     public static void closeConnection(Connection conn) {
         try {
-            conn.close();
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao fechar a conexão com o banco de dados: " + e.getMessage());
         }
     }
     
-    public static void closeStatement(Statement stmt) {
+    public static void closeStatement(PreparedStatement stmt) {
         try {
-            stmt.close();
+            if (stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao fechar a SQL Statement: " + e.getMessage());
         }
@@ -59,9 +63,17 @@ public class ConnectionFactory {
     
     public static void closeResultSet(ResultSet res) {
         try {
-            res.close();
+            if (res != null && !res.isClosed()) {
+                res.close();
+            }
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao fechar SQL ResultSet: " + e.getMessage());
         }
+    }
+    
+    public static void close(ResultSet res, PreparedStatement stmt, Connection conn) {
+        closeResultSet(res);
+        closeStatement(stmt);
+        closeConnection(conn);
     }
 }
